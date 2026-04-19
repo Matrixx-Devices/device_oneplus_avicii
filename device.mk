a54706269618c5fa6945183623908ea0528f9c7b
Pranav Vashi <neobuddy89@gmail.com>
2026-04-19 11:24:12 +0000

    avicii: overlay: Use appropriate charging thresholds * Our device directly measures in mA and not μA. * This fixes "Charging Rapidly" info on lockscreen.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -314,6 +314,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 PRODUCT_ENFORCE_Res_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigRes \
